4e0767f81fe142e8ff622953e44b3007adc4fb74
DavidDanielYam <daviddaniel67@hotmail.com>
2019-05-07 23:38:19 -0500

    Metodo Simpson

--- MetUnidad4/src/Principal1/FormmulasSimpson.java
+++ MetUnidad4/src/Principal1/FormmulasSimpson.java
@@ -3,5 +3,80 @@ package Principal1;
 
 public class FormmulasSimpson {
     
+    public double AnchoS(int b, int a, int nIterraciones)
+    {  
+         return (double) (b-a)/nIterraciones;
+        
+    }
+    
+    public double[] FuncionPreterminada(double Xn[], int a, int n){
+        double f[];
+        f=new double[n+1];
+        
+        for(int i=0;i<=n;i++){          
+                f[i]=Math.PI*(Math.pow(1+(Math.pow(Xn[i], 2)/2),2));
+        }
+        return f;
+    }
+    
+    public double[] XAumento(int a, double Ax, int n){
+        double acum[];
+        double sum=0;
+        acum=new double[n+1];
+        
+        for(int i=0;i<=n;i++){
+            if(i==0){
+                
+                acum[i]=a;
+            }
+            else{
+                sum+=Ax;
+                acum[i]=sum;
+            }               
+        }
+        return  acum;     
+    }
+    
+    public int[] MultiploFuncion( int n)
+    {
+        int Ki[] = new int[n+1];
+        
+        for(int i=0;i<=n;i++)
+        {
+            
+            if(i==0){
+                Ki[i]=1;
+            }else if(i==n){
+                Ki[i]=1;
+            }else if(i%2==0){
+                Ki[i]=2;
+            }else if(i%2!=0 && i!=0 && i!=n){
+                Ki[i]=4;
+            }
+        }
+        return Ki;
+    }
+    
+    public double[] K_funcion(int Ki[],double f[], int n){
+        double K_f[] = new double[n+1];
+        
+        for(int i=0;i<=n;i++){
+            K_f[i]=f[i]* Ki[i];
+        }
+        return K_f;
+    }
+    
+    public double SumaFunciones(double K_f[], int n){
+        double sum=0;
+        for(int i=0;i <=n;i++){
+            sum+=K_f[i];
+        }
+        return sum;
+    }
+    
+    public double resultadoFinal(double Ax,double suma){
+        
+        return (double) (Ax/3)*suma;
+    }
     
 }
--- MetUnidad4/src/Principal1/VentanaSimpson.java
+++ MetUnidad4/src/Principal1/VentanaSimpson.java
@@ -1,12 +1,22 @@
 
 package Principal1;
 
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class VentanaSimpson extends javax.swing.JFrame {
 
+    Object [] fila;
+    DefaultTableModel MoldeTabla;
+    FormmulasSimpson Etapa;
+    String head[]= {"Iteraciones", "Xn", "F(x)", "K", "K*f(x)"};
+    String data[][]={}; 
+    int i;
     
     public VentanaSimpson() {
         initComponents();
+        MoldeTabla= new DefaultTableModel(data,head);
+        TablaS.setModel(MoldeTabla);
     }
 
     
@@ -14,17 +24,147 @@ public class VentanaSimpson extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TablaS = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        LimB = new javax.swing.JLabel();
+        BLimit = new javax.swing.JTextField();
+        LimA = new javax.swing.JLabel();
+        ALimit = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        NVeces = new javax.swing.JTextField();
+        Ancho = new javax.swing.JLabel();
+        ancho = new javax.swing.JTextField();
+        BTCalcular = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        Nuevo = new javax.swing.JButton();
+        ResultadoF = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TablaS.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(TablaS);
+
+        jLabel1.setBackground(new java.awt.Color(51, 51, 51));
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT", 0, 18)); // NOI18N
+        jLabel1.setText("Metodo de Simpson");
+
+        jPanel1.setBackground(new java.awt.Color(102, 102, 102));
+
+        LimB.setText("LimiteB");
+
+        LimA.setText("LimiteA");
+
+        jLabel4.setText("N:");
+
+        Ancho.setText("Hancho");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LimA)
+                    .addComponent(LimB)
+                    .addComponent(jLabel4)
+                    .addComponent(Ancho))
+                .addGap(44, 44, 44)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(ancho, javax.swing.GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE)
+                    .addComponent(NVeces, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BLimit, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(ALimit, javax.swing.GroupLayout.Alignment.LEADING))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LimB)
+                    .addComponent(BLimit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LimA)
+                    .addComponent(ALimit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(NVeces, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Ancho)
+                    .addComponent(ancho, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(38, Short.MAX_VALUE))
+        );
+
+        BTCalcular.setText("Calcular");
+
+        jLabel2.setText("Resultado");
+
+        Nuevo.setText("Nuevo");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(112, 112, 112)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 402, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(BTCalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(29, 29, 29)
+                        .addComponent(Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(54, 54, 54)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(ResultadoF))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(65, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(BTCalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(Nuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(ResultadoF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(24, 24, 24))))))
         );
 
         pack();
@@ -34,5 +174,24 @@ public class VentanaSimpson extends javax.swing.JFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField ALimit;
+    private javax.swing.JLabel Ancho;
+    private javax.swing.JTextField BLimit;
+    private javax.swing.JButton BTCalcular;
+    private javax.swing.JLabel LimA;
+    private javax.swing.JLabel LimB;
+    private javax.swing.JTextField NVeces;
+    private javax.swing.JButton Nuevo;
+    private javax.swing.JTextField ResultadoF;
+    private javax.swing.JTable TablaS;
+    private javax.swing.JTextField ancho;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
+
+   
 }
